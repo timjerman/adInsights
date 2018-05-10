fe33b240b6fd241bd3b9b8cb773d1634be622d0b
TimJ <tim.jerman@gmail.com>
2018-05-10 19:28:33 +0200

    added histogram plotting of request timestamps

--- assignment.py
+++ assignment.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import dataLoader
 
@@ -8,11 +9,17 @@ def get_add_engagement(df: pd.DataFrame):
     df_engagement = df[['sessionId', 'name']].copy()
     df_engagement['interacted'] = (df_engagement['name'] == 'interaction') | (df_engagement['name'] == 'firstInteraction')
     session_group_engagement = df_engagement.groupby('sessionId').sum()
-    add_engagement_rate = 100 * np.count_nonzero(session_group_engagement['interacted']) / np.size(session_group_engagement['interacted'])
+    add_engagement_rate = 100 * np.count_nonzero(session_group_engagement['interacted']) / session_group_engagement['interacted'].size
     return add_engagement_rate
 
 
-
+def plot_add_requested_histogram(df: pd.DataFrame):
+    ad_requested_timestamps = df.loc[df['name'] == 'adRequested'][['clientTimestamp', 'timestamp']]
+    ad_requested_timestamps = df.loc[df['name'] == 'adRequested'][['clientTimestamp', 'timestamp']]
+    ad_requested_timestamps['clientTimestamp'] = pd.to_datetime(ad_requested_timestamps['clientTimestamp'], unit='s')
+    ad_requested_timestamps['timestamp'] = pd.to_datetime(ad_requested_timestamps['timestamp'], unit='s')
+    ad_requested_timestamps.groupby(pd.Grouper(key='clientTimestamp', freq='30Min')).count().plot(kind='bar')
+    plt.show()
 
 
 
@@ -23,6 +30,9 @@ file_name = 'bdsEventsSample.json'
 
 file_reader = dataLoader.FileReader(file_name)
 df = file_reader.loadData(use_already_preprocessed=True, save_preprocessed=True)
+
+# Exercise 1: plot histogram of ad request timestamps
+plot_add_requested_histogram(df)
 
 # Exercise 2: overall ad engagement rate
 add_engagement_rate = get_add_engagement(df)
