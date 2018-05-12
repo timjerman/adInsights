e6a3b6e60280feb816c1fb13c77b7f7f383b5c49
TimJ <tim.jerman@gmail.com>
2018-05-12 18:50:38 +0200

    added sorting of the input json file by server timestamp

--- dataLoader.py
+++ dataLoader.py
@@ -4,9 +4,11 @@ import os
 import time
 import datetime
 
+
 class FileReader:
 
     PROCESSED_SUFFIX = '_preprocessed'
+    SORTED_SUFFIX = '_sorted'
     in_file_name = ''
     processed_file_name = ''
 
@@ -28,18 +30,12 @@ class FileReader:
 
     def preprocess_data(self, df: pd.DataFrame):
 
-        # df = df.dropna(subset=['objectClazz'])
-        # tmp = df.groupby('name').count()
-        # df = df.loc[df['sdk'].notNull()]# == 'live']
-        # df = df.groupby('sessionId').filter(lambda g: any(g['purpose'] == 'live')).filter(lambda g: any(g['name'] == 'adRequested'))
-
         df['clientTimestamp'] = df['clientTimestamp'].fillna(value=0)
         # retain only selected values for column 'name'
         df = df.loc[df['name'].isin(['adRequested', 'screenShown', 'interaction', 'firstInteraction', 'userError', 'pageRequested'])].copy()
 
         # pageRequested doesn't have a client timestamp -> replace it with timestamp
         df.loc[df['name'] == 'pageRequested', 'clientTimestamp'] = df.loc[df['name'] == 'pageRequested', 'timestamp']
-        # df.loc[:, 'clientTimestamp'] = np.where(df['name'] == 'pageRequested', df['timestamp'], df['clientTimestamp'])
 
         # select only valid clientTimestamps - retain those that are inside of a 24 hour window to the valid server timestamps
         df = df.loc[
@@ -64,19 +60,38 @@ class FileReader:
 
         return df
 
-    def load_data(self, save_preprocessed=True, use_already_preprocessed=True):
+    def sort_by_timestamp(self, df: pd.DataFrame):
+        return df.sort_values(by=['timestamp'])
 
+    def load_and_preprocess_data(self, process_function, use_already_preprocessed=True, save_preprocessed=True):
         if use_already_preprocessed and os.path.exists(self.processed_file_name):
             return self.read_json_line_data(self.processed_file_name)
 
         df = self.read_json_line_data(self.in_file_name)
-        df = self.preprocess_data(df)
+        df = process_function(df)
 
         if save_preprocessed:
             self.write_json_line_data(df, self.processed_file_name)
 
         return df
 
+    def load_data(self, use_already_preprocessed=True, save_preprocessed=True):
+        df = self.load_and_preprocess_data(
+            self.preprocess_data,
+            use_already_preprocessed=use_already_preprocessed,
+            save_preprocessed=save_preprocessed
+        )
+
+        return df
+
+    def sort_input_data(self, use_already_preprocessed=True, save_preprocessed=True):
+        df = self.load_and_preprocess_data(
+            self.sort_by_timestamp,
+            use_already_preprocessed=use_already_preprocessed,
+            save_preprocessed=save_preprocessed
+        )
+
+        return df
 
 if __name__ == '__main__':
 
