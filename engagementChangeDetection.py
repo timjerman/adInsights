1a955ccb3df9e861cde671b28044b12e91a99698
TimJ <tim.jerman@gmail.com>
2018-05-12 21:08:28 +0200

    changed the way data in memory is updated and added the storage of object and sdk data for interactions

--- engagementChangeDetection.py
+++ engagementChangeDetection.py
@@ -7,53 +7,116 @@ import dataLoader
 
 class AdSession:
 
+    INTERACTED = 'interacted'
+    REQUESTED = 'requested'
+    SDK = 'sdk'
+    OBJECT = 'objectClazz'
+    TIMESTAMP = 'timestamp'
+    ERROR = 'userError'
+    LIVE = 'live'
+
     def __init__(self):
         self.data = {
-                'interacted': False,
-                'requested': False,
-                'sdk': '',
-                'objectClazz': '',
-                'timestamp': 0,
-                'error': False,
-                'live': False,
-                'wasInteractionUpdated': False
-            }
+            AdSession.INTERACTED: False,
+            AdSession.REQUESTED: False,
+            AdSession.SDK: '',
+            AdSession.OBJECT: '',
+            AdSession.TIMESTAMP: 0,
+            AdSession.ERROR: False,
+            AdSession.LIVE: False
+        }
 
-    def update(self, **kwargs):
+    def update(self, skip_if_exists=True, **kwargs):
         if kwargs is not None:
-            if not self.data['interacted'] and 'interacted' in kwargs.keys():
-                self.data['wasInteractionUpdated'] = True
             for key, value in kwargs.items():
-                self.data[key] = value
+                if not bool(self.data[key]) or not skip_if_exists:
+                    self.data[key] = value
 
         return self
+
+    def copy(self):
+        new_copy = AdSession().update(**self.data)
+        return new_copy
+
 
 
 class AdSessionWindow(OrderedDict):
     max_elements = 1
     engagement_rate = 0
     number_of_interactions = 0
+    error_count = 0
+    number_of_interactions_by_object = {}
+    number_of_interactions_by_sdk = {}
+
+    def __init__(self, max_elements=1, *args, **kwargs):
+        self.max_elements = max_elements
+        super().__init__(*args, **kwargs)
 
     def update_engagement_rate(self):
         self.engagement_rate = self.number_of_interactions / len(self)
 
-    def __init__(self, *args, max_elements=1, **kwargs):
-        self.max_elements = max_elements
-        super().__init__(*args, **kwargs)
+    def update_interaction_value(self, key, interaction_dict, add=True):
+        if key not in interaction_dict:
+            interaction_dict[key] = 0
+        if add:
+            interaction_dict[key] += 1
+        else:
+            interaction_dict[key] -= 1
+
+    def update_session_elements(self, session_id, **kwargs):
+
+        session_before_update = self[session_id].copy()
+        self[session_id] = self[session_id].update(skip_if_exists=True, **kwargs)
+        update_needed = False
+
+        if self[session_id].data[AdSession.INTERACTED]:
+            if session_before_update.data[AdSession.INTERACTED] != self[session_id].data[AdSession.INTERACTED]:
+                self.number_of_interactions += 1
+
+                if self[session_id].data[AdSession.SDK] != '':
+                    self.update_interaction_value(self[session_id].data[AdSession.SDK], self.number_of_interactions_by_sdk)
+                if self[session_id].data[AdSession.OBJECT] != '':
+                    self.update_interaction_value(self[session_id].data[AdSession.OBJECT], self.number_of_interactions_by_object)
+
+                update_needed = True
+            else:
+                if session_before_update.data[AdSession.SDK] == '' and self[session_id].data[AdSession.SDK] != '':
+                    self.update_interaction_value(self[session_id].data[AdSession.SDK], self.number_of_interactions_by_sdk)
+                    update_needed = True
+
+                if session_before_update.data[AdSession.OBJECT] == '' and self[session_id].data[AdSession.OBJECT] != '':
+                    self.update_interaction_value(self[session_id].data[AdSession.OBJECT], self.number_of_interactions_by_object)
+                    update_needed = True
+
+        if not session_before_update.data[AdSession.ERROR] and self[session_id].data[AdSession.ERROR]:
+            self.error_count += 1
+
+        if update_needed:
+            self.update_engagement_rate()
+
+
+        # self[session_id] = self[session_id].update(kwargs)
+
+
 
     def __setitem__(self, key, value: AdSession):
-        if value.data['wasInteractionUpdated'] and value.data['interacted']:
-            value.update(wasInteractionUpdated=False)
-            self.number_of_interactions += 1
-            self.update_engagement_rate()
 
         OrderedDict.__setitem__(self, key, value)
         if self.max_elements > 0:
             if len(self) > self.max_elements:
                 key_pop, value_pop = self.popitem(False)
-                if value_pop.data['interacted']:
+                if value_pop.data[AdSession.INTERACTED]:
                     self.number_of_interactions -= 1
-                    self.update_engagement_rate()
+                    if value_pop.data[AdSession.SDK] != '':
+                        self.update_interaction_value(value_pop.data[AdSession.SDK],
+                                                      self.number_of_interactions_by_sdk, add=False)
+                    if value_pop.data[AdSession.OBJECT] != '':
+                        self.update_interaction_value(value_pop.data[AdSession.OBJECT],
+                                                      self.number_of_interactions_by_object, add=False)
+                if value_pop.data[AdSession.ERROR]:
+                    self.error_count -= 1
+
+                self.update_engagement_rate()
 
 
 
@@ -61,6 +124,7 @@ if __name__ == '__main__':
 
     # load the data that will act as a stream and sort it by timestamp
     file_name = 'bdsEventsSample.json'
+    # file_name = 'test.json'
     file_reader = dataLoader.FileReader(file_name, suffix=dataLoader.FileReader.SORTED_SUFFIX)
     file_reader.sort_input_data(use_already_preprocessed=True, save_preprocessed=True)
     file_name = file_reader.processed_file_name
@@ -81,25 +145,40 @@ if __name__ == '__main__':
                 streamed_data_window[session_id] = AdSession()
 
             if stream_line['name'] == 'adRequested':
-                streamed_data_window[session_id] = streamed_data_window[session_id].update(
+                streamed_data_window.update_session_elements(
+                    session_id,
                     requested=True,
                     timestamp=stream_line['timestamp'],
                     live=stream_line['purpose'] == 'live'
                 )
             elif stream_line['name'] == 'interaction' or stream_line['name'] == 'firstInteraction':
-                streamed_data_window[session_id] = streamed_data_window[session_id].update(
+                streamed_data_window.update_session_elements(
+                    session_id,
                     interacted=True,
                     objectClazz=stream_line['objectClazz']
                 )
             elif stream_line['name'] == 'pageRequested':
-                streamed_data_window[session_id] = streamed_data_window[session_id].update(sdk=stream_line['sdk'])
+                streamed_data_window.update_session_elements(
+                    session_id,
+                    sdk=stream_line['sdk']
+                )
             elif stream_line['name'] == 'userError':
-                streamed_data_window[session_id] = streamed_data_window[session_id].update(userError=True)
+                streamed_data_window.update_session_elements(
+                    session_id,
+                    userError=True
+                )
 
             # print(streamed_data_window.engagement_rate)
             engagement_rate.append(streamed_data_window.engagement_rate)
 
     np.array(engagement_rate).dump('adEngagementRate.npy')
+
+    # # plots
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot(engagement_rate)
+    # plt.show()
 
 
     print('Finished!')
